adc369aa1df3736e4eac1cac922a890d85b55c7e
gajonuo <gajonuco@gmail.com>
2024-12-19 21:37:59 -0300

    Criado as entidades Coupon e Event

---
+++ src/main/java/com/eventostec/api/domain/address/Address.java
@@ -0,0 +1,5 @@
+package com.eventostec.api.domain.address;
+
+public class Address {
+    
+}
---
+++ src/main/java/com/eventostec/api/domain/coupon/Coupon.java
@@ -0,0 +1,40 @@
+package com.eventostec.api.domain.coupon;
+
+import java.util.Date;
+import java.util.UUID;
+
+import com.eventostec.api.domain.event.Event;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.GeneratedValue;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "coupon")
+@Entity
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+ 
+public class Coupon{
+    @Id
+    @GeneratedValue
+
+    private UUID id;
+
+    private String code;
+    private Integer discount;
+    private Date valid;
+
+    @ManyToOne
+    @JoinColumn(name = "event_id")
+    private Event event;
+}

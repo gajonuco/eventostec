13688e377782fbd262e6a155d617126c3591227a
gajonuo <gajonuco@gmail.com>
2024-12-27 21:50:38 -0300

    add CouponService; CouponController; CouponRequestDTO; EventResponseDTO

--- src/main/java/com/eventostec/api/controller/EventController.java
+++ src/main/java/com/eventostec/api/controller/EventController.java
@@ -2,11 +2,14 @@ package com.eventostec.api.controller;
 
 import com.eventostec.api.domain.event.Event;
 import com.eventostec.api.domain.event.EventRequestDTO;
+import com.eventostec.api.domain.event.EventResponseDTO;
 import com.eventostec.api.service.EventService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
@@ -28,5 +31,11 @@ public class EventController {
         EventRequestDTO eventRequestDTO = new EventRequestDTO(title,description, data, city, state, remote, eventUrl, image);
         Event newEvent = this.eventService.createEvent(eventRequestDTO);
         return ResponseEntity.ok(newEvent);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<EventResponseDTO>> getEvents(@RequestParam(defaultValue = "0") int page, @RequestParam(defaultValue = "10") int size){
+        List<EventResponseDTO> allEvents = this.eventService.getUpcomingEvents(page, size);
+                return ResponseEntity.ok(allEvents);
     }
 }
--- src/main/java/com/eventostec/api/repositories/EventRepository.java
+++ src/main/java/com/eventostec/api/repositories/EventRepository.java
@@ -1,9 +1,17 @@
 package com.eventostec.api.repositories;
 
+import java.util.Date;
 import java.util.UUID;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import com.eventostec.api.domain.event.Event;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface EventRepository extends JpaRepository<Event, UUID>{
-    
+
+    @Query("SELECT e FROM Event e WHERE e.date >= :currentDate")
+    public Page<Event> findUpcomingEvents(@Param("currentDate") Date currentDate, Pageable pageable);
 }
